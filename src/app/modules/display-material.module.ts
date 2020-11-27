1913e3aaa377f4ad73f6ffc13f9297386223dce2
Eric Luc <luckhoi89@gmail.com>
2020-11-26 20:41:27 -0800

    change home color

--- src/app/modules/display-material.module.ts
+++ src/app/modules/display-material.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider'
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button'; 
-import {MatIconModule} from '@angular/material/icon'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+ 
 @NgModule({
     imports:[
         MatSliderModule,
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatGridListModule
 
     ],
     exports:[
         MatSliderModule,
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatGridListModule
     ]
 })
 export class DisplayMaterialModule{
